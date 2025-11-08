71847a2706a2c20e30bd4f30705ebe27ff8eeb29
abdelmeenam <abdelmeenam@gmail.com>
2025-11-08 13:36:39 +0200

    feat: add 'Preview Products' button to clinic inventory page

--- resources/views/backend/dashboards/clinic/pages/clinic-inventories/index.blade.php
+++ resources/views/backend/dashboards/clinic/pages/clinic-inventories/index.blade.php
@@ -11,6 +11,12 @@
 						<i class="mdi mdi-plus"></i>
 						{{ __('Add Clinic Inventory') }}
 					</a>
+                    <!-- Preview Products -->
+                    <a href="{{ url(app()->getLocale().'/products') }}"
+                       class="btn btn-success">
+                        <i class="mdi mdi-cart-outline"></i>
+                        {{ __('Preview Products') }}
+                    </a>
 					<!-- Trash -->
 					<a href="{{ route('clinic.clinic-inventories.trash') }}"
 						class="btn btn-danger">
